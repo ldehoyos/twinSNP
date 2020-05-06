8d134326c8020bee4ac9ed02d62295bd0a5280e2
Lucía de Hoyos <luciadeh@gmail.com>
2020-05-06 22:05:30 +0200

    v0.1.1

---
+++ R/EURdata1-data.R
@@ -0,0 +1,19 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(EURdata1)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(EURdata1)
+#' myVars <- EURdata1$snpID
+"EURdata1"
---
+++ R/EURdata2-data.R
@@ -0,0 +1,19 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(EURdata2)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(EURdata2)
+#' myVars <- EURdata2$snpID
+"EURdata2"
